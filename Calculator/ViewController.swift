0f9d9e8b123060df1045263c30bceb520905d5da
Tarang <tsgoten@gmail.com>
2018-09-17 10:41:30 -0400

    added calculator buttons and layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,24 +2,23 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Tarang Srivastava on 9/7/18.
+//  Created by Tarang Srivastava on 9/7/18.      
 //  Copyright © 2018 Tarang Srivastava. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel?
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textCurrentlyInDisplay = display!.text!
+        display!.text = textCurrentlyInDisplay + digit
+        
+        // print("touched \(digit) digit")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
