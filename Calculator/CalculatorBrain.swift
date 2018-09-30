65325155b27a4bd8d2b17ad55c66b588d950dbf2
Tarang Srivastava <tsgoten@gmail.com>
2018-09-30 11:49:51 -0400

    Revert "some eroored changes"
    This reverts commit 0408f7bdf49a2a48ec88df839a1ba47533e1a2ea.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -15,11 +15,9 @@ func multiply(op1: Double, op2: Double) -> Double{
 class CalculatorBrain {
     
     private var accumulator = 0.0
-    private var internalProgram = [AnyObject]()
     
     func setOperand(operand: Double) {
         accumulator = operand
-        internalProgram.append(operand)
     }
     
     private var operations: Dictionary<String, Operation> = [
@@ -43,7 +41,6 @@ class CalculatorBrain {
     }
     
     func performOperation(symbol: String) {
-        internalProgram.append(symbol)
         if let operation = operations[symbol] {
             switch operation{
                 case .Constant(let value):
@@ -71,32 +68,6 @@ class CalculatorBrain {
     private struct PendingBinaryOperationInfo {
         var binaryFunction: (Double, Double) -> Double
         var firstOperand: Double
-    }
-    
-    typealias PropertyList = AnyObject
-    
-    var program: PropertyList {
-        get {
-            return internalProgram
-        }
-        set {
-            clear()
-            if let arrayOfOps = newValue as? [AnyObject] {
-                for op in arrayOfOps {
-                    if let op as? Double {
-                        setOperand(op)
-                    } else if let operation = op as? String {
-                        performOperation(operation)
-                    }
-                }
-            }
-        }
-    }
-    
-    func clear() {
-        accumulator = 0.0
-        pending = nil
-        internalProgram.removeAll()
     }
     
     var result: Double{
